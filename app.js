5db36e100ff1f2100648eefbb0c40eca55b1876f
Otto Borden <Otto.Borden@teamaol.com>
2014-07-22 10:13:25 -0400

    Fixed issue with body-parser urlencoded

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
